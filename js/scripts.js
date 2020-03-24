f7736541b76b47a76d7ed875763ba68a0fdea19c
Chisato Murakami <kiwipom1031@gmail.com>
2020-03-24 16:17:49 -0700

    Add actual toggle function!!

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
     $(".JavaScript-text").click(function(){
         $(".flashcard-show").toggle();
-        $(".flashcard-hidden").toggle();
+        // $(".flashcard-hidden").toggle();
     });
 
      $("#Operators-text").click(function(){
          $(".operator-show").toggle();
-         $(".operator-hide").toggle();
+        // $(".operator-hide").toggle();
      });
 
      $("#Varialble-text").click(function(){
         $(".variable-show").toggle();
-        $(".variable-hide").toggle();
+        //$(".variable-hide").toggle();
     });
 
      $("#Varialblename-text").click(function(){
          $(".variablename-show").toggle();
-         $(".variablename-hide").toggle();
+        // $(".variablename-hide").toggle();
      });
 });
