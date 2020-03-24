3c5155b41ac84b8ae0c966a48cb8f4d5d07df225
Chisato Murakami <kiwipom1031@gmail.com>
2020-03-24 14:58:09 -0700

    Add more functions with Joseph.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
-    $("p").click(function(){
+    $("#JavaScript-text").click(function(){
         $(".flashcard-show").toggle();
         $(".flashcard-hide").toggle();
     });
+
+     $("#Operators-text").click(function(){
+         $(".operator-show").toggle();
+         $(".operator-hide").toggle();
+     });
+
 });
